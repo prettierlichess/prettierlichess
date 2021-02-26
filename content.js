ca74a4a7320c9fb7ab39ba5d7e85b47460d1789d
Durush <Demonto123@gmail.com>
2021-02-26 09:02:08 -0500

    make the code just a bit gooder

--- content.js
+++ content.js
@@ -1,28 +1,12 @@
-chrome.storage.sync.get("primaryColor", function (result) {
-    document.documentElement.setAttribute("style", "--green: " + result["primaryColor"] + " !important;");
-    console.log(result["primaryColor"]);
-});
+const colorScheme = ["primaryColor","secondaryColor","tertiaryColor","backgroundColor","surfaceColor","surfaceColorHover","defaultWhite"]
 
-chrome.storage.sync.get("secondaryColor", function (result) {
-    document.documentElement.setAttribute("style", document.documentElement.getAttribute('style') + "--orange: " + result["secondaryColor"] + " !important;");
-});
+colorScheme.forEach(scheme => schemeSet(scheme));
 
-chrome.storage.sync.get("tertiaryColor", function (result) {
-    document.documentElement.setAttribute("style", document.documentElement.getAttribute('style') + "--red: " + result["tertiaryColor"] + " !important;");
-});
-
-chrome.storage.sync.get("backgroundColor", function (result) {
-    document.documentElement.setAttribute("style", document.documentElement.getAttribute('style') + "--gray-dark: " + result["backgroundColor"] + " !important;");
-});
-
-chrome.storage.sync.get("surfaceColor", function (result) {
-    document.documentElement.setAttribute("style", document.documentElement.getAttribute('style') + "--gray-light: " + result["surfaceColor"] + " !important;");
-});
-
-chrome.storage.sync.get("surfaceColorHover", function (result) {
-    document.documentElement.setAttribute("style", document.documentElement.getAttribute('style') + "--gray-light-hover: " + result["surfaceColorHover"] + " !important;");
-});
-
-chrome.storage.sync.get("defaultWhite", function (result) {
-    document.documentElement.setAttribute("style", document.documentElement.getAttribute('style') + "--white: " + result["defaultWhite"] + " !important;");
-});
+function schemeSet(scheme) {
+    chrome.storage.sync.get(scheme, function (result) {
+        console.log(scheme+" : " + result[scheme]);
+        let getStyle = document.documentElement.getAttribute('style');
+        let appendStyle = getStyle ? getStyle : '';
+        document.documentElement.setAttribute("style", `${appendStyle} --${scheme}: ${result[scheme]} !important;`);
+    });
+}
