5a89a84c784838b91e152500b81d83803b51e3d0
Noah Semus <33107099+noahsemus@users.noreply.github.com>
2021-02-26 10:42:26 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,12 +1,14 @@
-const colorScheme = ["primaryColor","secondaryColor","tertiaryColor","backgroundColor","surfaceColor","surfaceColorHover","defaultWhite"]
+const colorScheme = ["primaryColor", "secondaryColor", "tertiaryColor", "backgroundColor", "surfaceColor", "surfaceColorHover", "defaultWhite"]
 
 colorScheme.forEach(scheme => schemeSet(scheme));
 
 function schemeSet(scheme) {
     chrome.storage.sync.get(scheme, function (result) {
-        console.log(scheme+" : " + result[scheme]);
-        let getStyle = document.documentElement.getAttribute('style');
-        let appendStyle = getStyle ? getStyle : '';
-        document.documentElement.setAttribute("style", `${appendStyle} --${scheme}: ${result[scheme]} !important;`);
+        console.log(scheme + " : " + result[scheme]);
+        if (result[scheme]) {
+            let getStyle = document.documentElement.getAttribute('style');
+            let appendStyle = getStyle ? getStyle : '';
+            document.documentElement.setAttribute("style", `${appendStyle} --${scheme}: ${result[scheme]} !important;`);
+        }
     });
 }
