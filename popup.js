febd8af0dcef187cc4eebbd1c392ffbe3c02a401
Durush <47361958+Durush@users.noreply.github.com>
2021-02-28 13:57:51 -0500

    new color picker

--- popup.js
+++ popup.js
@@ -1,53 +1,16 @@
 const colorScheme = ["primaryColor", "secondaryColor", "tertiaryColor", "backgroundColor", "surfaceColor", "surfaceColorHover", "defaultWhite"]
 const colorDefaults = ["#9FC0A2", "#f5c276", "#d36d6d", "#2B343B", "#3F474D", "#4e565c", "#F2F5F3"]
 
-const primaryColor = document.querySelector('#primaryColor');
 
-const pickr = new Pickr({
-    el: primaryColor,
-    default: '#42445A',
-    theme: 'nano',
-
-    swatches: [
-        'rgba(244, 67, 54, 1)',
-        'rgba(233, 30, 99, 0.95)',
-        'rgba(156, 39, 176, 0.9)',
-        'rgba(103, 58, 183, 0.85)',
-        'rgba(63, 81, 181, 0.8)',
-        'rgba(33, 150, 243, 0.75)',
-        'rgba(3, 169, 244, 0.7)',
-        'rgba(0, 188, 212, 0.7)',
-        'rgba(0, 150, 136, 0.75)',
-        'rgba(76, 175, 80, 0.8)',
-        'rgba(139, 195, 74, 0.85)',
-        'rgba(205, 220, 57, 0.9)',
-        'rgba(255, 235, 59, 0.95)',
-        'rgba(255, 193, 7, 1)'
-    ],
-
-    components: {
-        preview: true,
-        opacity: true,
-        hue: true,
-
-        interaction: {
-            hex: true,
-            rgba: true,
-            hsva: true,
-            input: true,
-            clear: true,
-            save: true
-        }
-    }
-});
-
-colorScheme.forEach(scheme => colorPickerSet(scheme));
-colorScheme.forEach(scheme => addChangeListener(scheme));
+var j;
+for(j = 0; j < colorScheme.length; j++){
+    pickrCreate(colorScheme[j], colorDefaults[j]);
+}
 
 document.querySelector('#resetButton').addEventListener('click', () => {
-    document.getElementById("list").reset();
 
     let scheme = null
+    var i;
     for (i = 0; i < colorScheme.length; i++) {
         scheme = colorScheme[i]
         chrome.storage.sync.set({
@@ -57,28 +20,42 @@ document.querySelector('#resetButton').addEventListener('click', () => {
     tabScript('window.location.reload();');
 })
 
-function addChangeListener(scheme) {
-    let schemeElement = document.getElementById(scheme);
-
-    schemeElement.addEventListener('change', (element) => {
-        let color = element.target.value;
-        let schemeCode = 'document.documentElement.setAttribute("style", (document.documentElement.getAttribute("style") ? document.documentElement.getAttribute("style") : "") + "--' + scheme + ': ' + color + ' !important;")'
-
-        tabScript(schemeCode);
-
-        chrome.storage.sync.set({
-            [scheme]: color
-        });
-    });
-}
-
-function colorPickerSet(scheme) {
-    let schemeElement = document.getElementById(scheme);
-
+function pickrCreate(scheme, color){
     chrome.storage.sync.get(scheme, function (result) {
-        if (result[scheme]) {
-            schemeElement.setAttribute('value', result[scheme]);
-        }
+        color = result[scheme] ? result[scheme] : color;
+
+        let schemeElement = document.querySelector(`#${scheme}`);
+        let pickr = new Pickr({
+            el: schemeElement,
+            default: color,
+            theme: 'nano',
+
+            components: {
+                preview: true,
+                opacity: true,
+                hue: true,
+
+                interaction: {
+                    hex: true,
+                    input: true,
+                    save: true
+                }
+            }
+        });
+
+        pickr.on('save', (color) =>{
+            color = color.toHEXA().toString();
+            console.log('setting color to: ' + color)
+            console.log(typeof color)
+            let schemeCode = 'document.documentElement.setAttribute("style", (document.documentElement.getAttribute("style") ? document.documentElement.getAttribute("style") : "") + "--' + scheme + ': ' + color + ' !important;")'
+
+            tabScript(schemeCode);
+
+            chrome.storage.sync.set({
+                [scheme]: color
+            });
+
+        });
     });
 }
 
