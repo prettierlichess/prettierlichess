c415563c3a41741b06a48ace68faa798574ba471
Noah Semus <33107099+noahsemus@users.noreply.github.com>
2021-05-11 11:48:22 -0400

    Changed fonts to default web fonts
    rip red hat display

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 const colorScheme = ["primaryColor", "secondaryColor", "tertiaryColor", "backgroundColor", "surfaceColor", "surfaceColorHover", "defaultWhite", "arrowPrimary", "arrowSecondary", "arrowTertiary", "arrowAlternate", "lastMove", "preMove", "moveIndicator", "boardDark", "boardLight"]
-const colorDefaults = ["#9FC0A2", "#f5c276", "#d36d6d", "#2B343B", "#3F474D", "#4e565c", "#F2F5F3", "#68C07B", "#f3ae48", "#d64d4d", "#4b81e6", "#99d69e", "#487292", "#99d69e", "#71828F", "#c7c7c7"]
+const colorDefaults = ["#9FC0A2", "#f5c276", "#d36d6d", "#2B343B", "#3F474D", "#4e565c", "#F2F5F3", "#68C07B", "#f3ae48", "#d64d4d", "#4b81e6", "#99d69e", "#1B7CC7", "#99d69e", "#71828F", "#c7c7c7"]
 const styleTernary = 'document.documentElement.setAttribute("style", (document.documentElement.getAttribute("style") ? document.documentElement.getAttribute("style") : "") + "--'
 const basicImportExportContainer = document.getElementById('basicImportExportContainer')
 const basicImportExportInput = document.getElementById('basicImportExport')
@@ -161,8 +161,7 @@ document.querySelector('#exportButton').addEventListener('click', () => {
             chrome.downloads.download({
                 url: url // The object URL can be used as download URL
             });
-        }
-        else {
+        } else {
             alert("No custom colors have been set.")
         }
     });
@@ -247,8 +246,8 @@ function tabScript(code) {
     }, function (tabs) {
         chrome.tabs.executeScript(
             tabs[0].id, {
-            code: code
-        });
+                code: code
+            });
     });
 }
 
