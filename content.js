15e21a8e5003a96d890d6d0cdb37fe5011d0f920
Noah Semus <33107099+noahsemus@users.noreply.github.com>
2021-05-16 11:04:45 -0400

    init animation separation

--- content.js
+++ content.js
@@ -13,37 +13,6 @@ function schemeSet(scheme) {
     });
 }
 
-//Top Navigation
-
-const tabSelector = document.querySelectorAll('#topnav section>a');
-const tabGroup = document.querySelectorAll('[role="group"]');
-
-tabSelector.forEach((item, index) => {
-    item.addEventListener('mouseenter', e => {
-        if (item.nextSibling == tabGroup[index]) {
-            item.nextSibling.classList.add('topnavHover');
-        }
-    })
-
-    item.addEventListener('mouseleave', e => {
-        if (item.nextSibling == tabGroup[index]) {
-            item.nextSibling.classList.remove('topnavHover');
-        }
-    })
-
-    item.nextSibling.addEventListener('mouseenter', e => {
-        if (item.nextSibling == tabGroup[index]) {
-            item.nextSibling.classList.add('topnavHover');
-        }
-    })
-
-    item.nextSibling.addEventListener('mouseleave', e => {
-        if (item.nextSibling == tabGroup[index]) {
-            item.nextSibling.classList.remove('topnavHover');
-        }
-    })
-})
-
 // Coordinates
 
 //Check if using default board
