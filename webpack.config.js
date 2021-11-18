e705edc60beb65420c921384afc958338ba60166
Pierre-Adrien <pabuisson@gmail.com>
2021-11-18 09:00:29 +0100

    add clean-webpack-plugin to clean the build folder before rebuilding

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "production",
   entry: {
-    popup: "./src/popup.js"
+    popup: "./src/popup.js",
   },
-  output: {},
+  output: {
+    path: path.resolve(process.cwd(), "dist"),
+  },
   plugins: [
+    new CleanWebpackPlugin(),
     new CopyPlugin({
       patterns: [
         {
           from: "src",
           to: ".",
           globOptions: {
-            ignore: ['./popup.js']
-          }
+            ignore: ["./popup.js"],
+          },
         },
         {
           from: "./node_modules/@simonwep/pickr/dist/themes/nano.min.css",
           to: "./nano.min.css",
-        }
+        },
       ],
     }),
-  ]
+  ],
 };
